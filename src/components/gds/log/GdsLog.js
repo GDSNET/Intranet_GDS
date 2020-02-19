0fce2209c5dbba7ea882ae865a3da11fe0010e92
mstone <mstone@gdsnet.com>
2020-02-19 12:23:07 -0300

    sincoMartin
    ahiva

--- src/components/gds/log/GdsLog.js
+++ src/components/gds/log/GdsLog.js
@@ -2,21 +2,35 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import combinaActions from "../../../actions/index";
 import {bindActionCreators} from 'redux';
-import {Text} from 'react-native-web'
+import {Text, View, StyleSheet} from 'react-native-web'
+import GdsPicker from './GdsPicker'
 
  
 class Cliente extends Component {
 
+  funEjemplo (){
+    alert('ejemplo picker')
+  }
 
   render() {
     const {nombre, funCambiaNombre,  } = this.props;
     return (
-      <div>
+      <View style={styles.container}>
         <Text>
           Desarrollando Log
         </Text>
+
+     
+
+    <GdsPicker funExec={this.funEjemplo} />
+    <GdsPicker funExec={this.funEjemplo} />
+    <GdsPicker funExec={this.funEjemplo} />
+    <GdsPicker funExec={this.funEjemplo} />
+
+
+
            
-      </div>
+      </View>
     );
   }
 }
@@ -43,3 +57,13 @@ return bindActionCreators(
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cliente);
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    flexDirection: 'row'
+    
+  },
+  
+});
